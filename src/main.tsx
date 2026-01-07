a4eb18c5b1b5ab5d5a766dd2b3933e84e334816d
André Cordeiro <andre_cordeiro@outlook.com.br>
2026-01-07 17:45:03 -0300

    build: remove top-level await in main.tsx; chore: ignore .npm cache; ui: show AIH Avançado in LEAN; feat: order patients by discharge date; fix: patient upsert by hospital_id,cns

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,9 @@ import './index.css'
 import './components/index.css'
 import { LEAN_MODE } from './config/system'
 
-if (!LEAN_MODE) {
-  await import('./config/doctorPaymentRules/preload')
-}
-
-createRoot(document.getElementById("root")!).render(<App />);
+;(async () => {
+  if (!LEAN_MODE) {
+    await import('./config/doctorPaymentRules/preload')
+  }
+  createRoot(document.getElementById("root")!).render(<App />)
+})()
